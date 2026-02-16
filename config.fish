03b3f4ef6118df3d850037d4fdcecec3fc4c59e4
AndroidGeeksYT <xavierlibresanora@gmail.com>
2026-02-16 17:16:18 +0800

    use tmux session after fastfetch

--- config.fish
+++ config.fish
@@ -1,4 +1,11 @@
 if status is-interactive
 # Commands to run in interactive sessions can go here
+
+	# Show system info first.
 	fastfetch
+	
+	# Run tmux session.
+	if not set -q TMUX
+		exec tmux
+	end
 end
